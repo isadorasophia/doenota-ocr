a311e42521114976eb08537f38ffc29f1e380417
Isadora <isaecia@gmail.com>
2015-07-13 22:16:58 -0300

    extractData script improved.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import base64
 import os
+import shutil
 
 from scripts.DB import *
 from scripts.extractData import *
@@ -17,40 +18,63 @@ class ReceiptModel():
 		pass
 
 	# saves the raw receipt to further use
-	def saveReceipt (self):
-		rawPath = rawDir + self.data_id + extension
+	def saveReceipt (self, idDir):
+		rawPath = rawDir + idDir + self.data_id + extension
 
 		with open(rawPath, "wb") as f:
 			f.write(base64.decodestring(self.image))
 			f.close()
 
 	@staticmethod
-	def createWorkspace():
+	def createWorkspace(idDir):
 		if not os.path.exists(rawDir):
-	    	os.makedirs(rawDir)
+	    	os.makedirs(rawDir + idDir)
 
 		if not os.path.exists(processedDir):
-	    	os.makedirs(processedDir)
+	    	os.makedirs(processedDir + idDir)
 
 		if not os.path.exists(resultsDir):
-	    	os.makedirs(resultsDir)
+	    	os.makedirs(resultsDir + idDir)
+
+	@staticmethod
+	def cleanWorkspace(idDir):
+		shutil.rmtree(rawDir + idDir)
+		shutil.rmtree(processedDir + idDir)
+		shutil.rmtree(resultsDir + idDir)
 
 if __name__ == "__main__":
-	createWorkspace()
     database = DBop()
 
     # build initial data
     receipt = ReceiptModel()
     receipt.data_id, receipt.image = database.getNextImage()
 
+    # local directory for data handling
+    idDir = receipt.data_id + '/'
+
+    createWorkspace(idDir)
+
 	# saves the image
-	# receipt.saveReceipt()
+	# receipt.saveReceipt(idDir)
 
 	# process receipt with the respective algorithms
-	processor = Processor(rawDir, processedDir, receipt.data_id, extension)
+	processor = Processor(rawDir + idDir, processedDir + idDir, receipt.data_id, extension)
 	processor.CRSB()
 	processor.CTS()
 
 	# perform ocr on the processed images
-	ocr = OCR(processedDir)
-	ocr.performOCR(resultsDir)
+	ocr = OCR(processedDir + idDir)
+	ocr.performOCR(resultsDir + idDir)
+
+	extractor = DataExtractor(resultsDir + idDir)
+
+	# perform data extract
+	receipt.COO = extractor.COO()
+	receipt.CNPJ = extractor.CNPJ()
+	receipt.date = extractor.date()
+	receipt.total = extractor.total()
+
+	database.save(receipt.data_id, receipt.CNPJ, receipt.date, receipt.COO, 
+		receipt.total)
+
+	receipt.cleanWorkspace(idDir)
--- scripts/extractData.py
+++ scripts/extractData.py
@@ -1,6 +1,9 @@
 import os
 import re
 
+CURRENT_YEAR = '2015'
+CNPJ_NUM = 5
+
 class DataExtractor():
 	def __init__(self, path):
 		files = [path + f for f in os.listdir(path)]
@@ -12,32 +15,108 @@ class DataExtractor():
 				self.contents += [txt.read()]
 
 	def CNPJ(self):
-		return
-
-	def COO(self):
-		return
-
-	def date(self):
-		dates = []
+		CNPJ = [{'' : 0} for k in range(CNPJ_SIZE)]
 
 		for content in self.contents:
-			if re.search('../../....', content) != None:
-				begin, end = re.search('../../....', content).span()
-				
-				candidate = content[begin:end]
+			try:
+				t = re.search('..[,.$-/]\s*...[,.$-/]\s*.../\s*....[,.$-/]\s*.*', content).group(0)
 
-				day = self.onlyNumbers(candidate[0:2])
-				month = self.onlyNumbers(candidate[3:5])
-				year = 20 + self.onlyNumbers(candidate[8:])
+				t = onlyNumbers(t)
 
-				candidate = day + "/" + month + "/" + year
+				# separate numbers into groups and alocate them
+				counter = 0
+				for k in re.findall(r'\d+', t):
+					CNPJ[counter][k] += 1 if k in CNPJ.viewvalues() else CNPJ[counter][k] = 1
 
-				dates += [candidate]
+					counter += 1 if counter + 1 < CNPJ_SIZE else pass
 
-		for candidate in dates
+			except IndexError:
+				pass
+
+		return '.'.join(maxValue(CNPJ[i]) for i in range (CNPJ_SIZE))
+
+	def COO(self):
+		COO = {'' : 0}
+
+		for content in self.contents:
+			try:
+				t = re.search('C00:......|CÓÓ:......|COO:......|00:......|00\.......|cUU:......', content).group(0)
+
+				t = onlyNumbers(t[len (t) - 6:])
+
+				COO[t] += 1 if (t in COO.viewvalues()) else COO[t] = 1
+			except IndexError:
+				pass
+
+		return maxValue(COO)
+
+	def date(self):
+		days = months = {'' : 0}
+		years = {CURRENT_YEAR : 1}
+		date = ''
+
+		for content in self.contents:
+			content = re.compile(content)
+
+			try:
+				t = re.search('../../....', content).group(0)
+
+				# get positions since is a fixed position by the regular exp
+				day = onlyNumbers(t[0:2])
+				month = onlyNumbers(t[3:5])
+				year = onlyNumbers(t[6:])
+
+				days[day] += 1 if (day in days.viewvalues()) else days[day] = 1
+				months[month] += 1 if (month in months.viewvalues()) else months[month] = 1
+
+				# since initial value still have an advantage, it is set to 0
+				years[year] += 1 if (year in years.viewvalues()) else years[year] = 0
+			except IndexError:
+				pass
+
+		date = maxValue(days) + '/' + maxValue(month) + '/' + maxValue (years)
+
+		return date
 
 	def total(self):
-		return
+		total = {'' : 0}
+
+		for content in self.contents:
+			try:
+				t = re.search('T.*T.*L[^\\n]|TAL R$[^\\n]|TAL RS[^\\n]|$ .*[,.$]\s*..', content).group(0)
+
+				# \s* in order to ignore spaces
+				t = onlyNumbers(re.search('.*[,.$]\s*..').group(0))
+				t = ','.join[srt(i) for i in re.findall(r'\d+', t)]
+
+				total[t] += 1 if (t in total.viewvalues()) else total[t] = 1
+			except IndexError:
+				pass
+
+		return maxValue(total)
+
+	# tries to replace at most the possible OCR misassumptions, 
+	# since we are only dealing with numbers
+	@staticmethod
+	def onlyNumbers (s):
+		dic = {'l':'1', 'D':'0', 'O':'0', '/':'', '-':''}
+
+		s = replaceAll(s, dic)
+		return s
+
+	# replace all occurrences of a key with its value
+	@staticmethod
+	def replaceAll (s, dic):
+		for i, j in dic.iteritems():
+			s = s.replace(i, j)
+
+		return s
+
+	# returns max value in a dict
+	@staticmethod
+	def maxValue (dic):
+		v = list(dic.values())
+		return max(v)
 
 if __name__ == "__main__":
 	extractor = DataExtractor('/home/isadora/projects/doenota-ocr/scripts/temp/')
